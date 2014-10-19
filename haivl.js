459b85945c5d03f4fc1d0666b0b37684bee02fa8
nguyenvinhlinh <nguyenvinhlinh@zing.vn>
2014-10-19 14:51:47 +0700

    add haivl.js

---
+++ haivl.js
@@ -0,0 +1,12 @@
+require("content-buffer.js");
+define_keymap("haivl_keymap",$display_name="haivl keymap");
+define_key(haivl_keymap, "tab",null,$fallthrough);
+
+define_keymaps_page_mode("haivl-mode", 
+       build_url_regexp($domain="haivl",
+					    $allow_www = true,
+					    $tlds = ["com"]),
+       {normal: haivl_keymap},
+       $display_name = "haivl");
+//to active this mode automatically
+page_mode_activate(haivl-mode);
