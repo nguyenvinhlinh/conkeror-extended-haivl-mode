59f0fbea7b6ef038133cf21cf1bcfbe089dc49b9
nguyenvinhlinh <nguyenvinhlinh@zing.vn>
2014-10-19 15:06:08 +0700

    found error: char '-'fixed and now it can automatically swith to the mode

--- haivl.js
+++ haivl.js
@@ -2,11 +2,11 @@ require("content-buffer.js");
 define_keymap("haivl_keymap",$display_name="haivl keymap");
 define_key(haivl_keymap, "tab",null,$fallthrough);
 
-define_keymaps_page_mode("haivl-mode", 
+define_keymaps_page_mode("haivl_mode", 
        build_url_regexp($domain="haivl",
 					    $allow_www = true,
 					    $tlds = ["com"]),
        {normal: haivl_keymap},
        $display_name = "haivl");
 //to active this mode automatically
-page_mode_activate(haivl-mode);
+page_mode_activate(haivl_mode);
