ef94d0c1b3f83973d24d972911a4a388c1df1735
nguyenvinhlinh <nguyenvinhlinh@zing.vn>
2014-10-22 11:23:55 +0700

    fixed Z X

--- haivl.js
+++ haivl.js
@@ -1,6 +1,9 @@
 require("content-buffer.js");
 define_keymap("haivl_keymap",$display_name="haivl keymap");
 define_key(haivl_keymap, "tab",null,$fallthrough);
+define_key(haivl_keymap, "z",null,$fallthrough);
+define_key(haivl_keymap, "x",null,$fallthrough);
+
 
 define_keymaps_page_mode("haivl_mode", 
        build_url_regexp($domain="haivl",
@@ -10,3 +13,4 @@ define_keymaps_page_mode("haivl_mode",
        $display_name = "haivl");
 //to active this mode automatically
 page_mode_activate(haivl_mode);
+var haivl = {};
